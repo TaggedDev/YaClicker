e889ae8a5ed3cb5ca1608f8438d0d96ad9aaba3e
Mikov Denis <denis.mikov.06@gmail.com>
2023-09-05 11:37:44 +0300

    feat(yandex): leaderboard

---
+++ Assets/Scripts/Yandex/Leaderboard.cs
@@ -0,0 +1,27 @@
+﻿using Economy;
+using UnityEngine;
+using YG;
+
+namespace Yandex
+{
+    public class Leaderboard : MonoBehaviour
+    {
+        [SerializeField] private PlayerResource resource;
+        private float _timer = 2;
+        
+        private void Update()
+        {
+            _timer -= Time.deltaTime;
+            if (_timer > 0)
+                return;
+
+            WriteDataToLeaderboard();
+            _timer = 2;
+        }
+
+        private void WriteDataToLeaderboard()
+        {
+            YandexGame.NewLeaderboardScores("coins", (int)resource.ResourceBank);
+        }
+    }
+}
